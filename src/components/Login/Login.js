84bf31dd52bbb304df0ca08a474c6960117f1e08
Ramirez Salinas <WKYV6AN@vw.vwgroup.com>
2023-09-12 16:01:09 -0600

    Siguiente avance como dos videos

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 import { act } from 'react-dom/test-utils';
 
 const emailReducer = (state, action) => {
@@ -42,6 +43,8 @@ const Login = (props) => {
     isValid: null,
   } );
 
+  const authCtx = useContext(AuthContext);
+
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
@@ -77,7 +80,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
